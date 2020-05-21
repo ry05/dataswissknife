719459ff335bea34268887584d53c93fce184b60
Ramshankar Yadhunath <yadramshankar@gmail.com>
2020-05-21 10:59:27 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setuptools.setup(
     version="0.0.1",
     author="Ramshankar Yadhunath, Srivenkata Srikanth, Arvind Sudheer",
     author_email="yadramshankar@gmail.com",
-    description="Automating Data Science for Domain Experts",
+    description="A Handy Little Tool to aid your Data Science Project",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/ry05/dataswissknife",
