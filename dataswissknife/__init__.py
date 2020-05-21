40bdaa7dff46fa2540ebdef75216db1ea540723a
Ramshankar Yadhunath <yadramshankar@gmail.com>
2020-05-21 06:13:15 +0530

    modify paths

---
+++ dataswissknife/__init__.py
@@ -0,0 +1,4 @@
+# not empty
+
+# currently, this file is not needed
+# will be updated when converting this to a package
--- setup.py
+++ setup.py
@@ -1,1 +1,22 @@
-# not empty
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="dataswissknife",
+    version="0.0.1",
+    author="Ramshankar Yadhunath, Srivenkata Srikanth, Arvind Sudheer",
+    author_email="yadramshankar@gmail.com",
+    description="Automating Data Science for Domain Experts",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/ry05/dataswissknife",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
